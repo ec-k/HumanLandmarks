b5d2c45af0fe5c5fb998ffcda7e9b14d79cde117
EC-K <elxone.19jt@gmail.com>
2025-12-30 22:01:28 +0900

    chore: update build_proto.ps1

--- build_proto.ps1
+++ build_proto.ps1
@@ -1,38 +1,36 @@
 Write-Host "Generating Protobuf code..."
 
-$PROTO_PATH = "./Proto/"
-$PYTHON_OUT_DIR = ".\Runtime\Python"
-$CSHARP_OUT_DIR = ".\Runtime\Csharp"
+$PROTO_PATH = ".\Proto\"
+$OUT_DIR = ".\Runtime\"
 
-# Create proto_generated directory for Python, similar to 'mkdir || echo' but more PowerShell idiomatic.
-if (-not (Test-Path -Path $PYTHON_OUT_DIR -PathType Container)){
-    New-Item -ItemType Directory -Path $PYTHON_OUT_DIR | Out-Null
+# Docker image settings for proto compiler
+$DOCKERFILE_PATH = ".\tools\protobuf-codegen\Dockerfile"
+$PROTO_VERSION = "25.3"
+$PROTO_IMAGE_NAME = "protoc-generator:$PROTO_VERSION"
+
+# Create output directory if it doesn't exist
+if (-not (Test-Path -Path $OUT_DIR -PathType Container)){
+    New-Item -ItemType Directory -Path $OUT_DIR | Out-Null
 }
 
-# Create C# output directory if it doesn't exist
-if (-not (Test-Path -Path $CSHARP_OUT_DIR -PathType Container)){
-    New-Item -ItemType Directory -Path $CSHARP_OUT_DIR | Out-Null
-}
+Write-Host "Building Docker image '$PROTO_IMAGE_NAME' from '$DOCKERFILE_PATH'..."
+docker build -f $DOCKERFILE_PATH -t $PROTO_IMAGE_NAME . --build-arg PROTOBUF_VERSION=$PROTO_VERSION
 
-Write-Host "Generating Python code..."
-protoc -I"$PROTO_PATH" --python_out="$PYTHON_OUT_DIR" "landmark.proto"
-protoc -I"$PROTO_PATH" --python_out="$PYTHON_OUT_DIR" "kinect_pose_landmarks.proto"
-protoc -I"$PROTO_PATH" --python_out="$PYTHON_OUT_DIR" "pose_landmarks.proto"
-protoc -I"$PROTO_PATH" --python_out="$PYTHON_OUT_DIR" "hand_landmarks.proto"
-protoc -I"$PROTO_PATH" --python_out="$PYTHON_OUT_DIR" "face_results.proto"
-protoc -I"$PROTO_PATH" --python_out="$PYTHON_OUT_DIR" "holistic_landmarks.proto"
-protoc -I"$PROTO_PATH" --python_out="$PYTHON_OUT_DIR" "landmark_log.proto"
-
-# Create an empty __init__.py file
-New-Item -Path "$PYTHON_OUT_DIR\__init__.py" -ItemType File -Force | Out-Null
+# Get .proto files
+$protoFiles = Get-ChildItem -Path $PROTO_PATH -Filter "*.proto" | ForEach-Object { $_.Name }
 
 Write-Host "Generating C# code..."
-protoc -I"$PROTO_PATH" --csharp_out="$CSHARP_OUT_DIR" "landmark.proto"
-protoc -I"$PROTO_PATH" --csharp_out="$CSHARP_OUT_DIR" "kinect_pose_landmarks.proto"
-protoc -I"$PROTO_PATH" --csharp_out="$CSHARP_OUT_DIR" "pose_landmarks.proto"
-protoc -I"$PROTO_PATH" --csharp_out="$CSHARP_OUT_DIR" "hand_landmarks.proto"
-protoc -I"$PROTO_PATH" --csharp_out="$CSHARP_OUT_DIR" "face_results.proto"
-protoc -I"$PROTO_PATH" --csharp_out="$CSHARP_OUT_DIR" "holistic_landmarks.proto"
-protoc -I"$PROTO_PATH" --csharp_out="$CSHARP_OUT_DIR" "landmark_log.proto"
+foreach ($file in $protoFiles) {
+    Write-Host "  Generating C# for $file"
+
+    $volumePathProto = (Convert-Path $PROTO_PATH).Replace('\', '/')
+    $volumePathOut = (Convert-Path $OUT_DIR).Replace('\', '/')
+
+    docker run --rm `
+        -v `"$volumePathProto`":/app/proto `
+        -v `"$volumePathOut`":/app/csharp_out `
+        $PROTO_IMAGE_NAME `
+        protoc -I/app/proto --csharp_out=/app/csharp_out /app/proto/$file
+}
 
 Write-Host "Done."
